b2817f8a7ed606cf15db32736dfc4d1db13d32b6
a-rya13 <aryaagarwal20031@gmail.com>
2025-09-30 00:47:09 +0530

    pushing

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 // ✅ Enable CORS so frontend (5173) can call backend (5000)
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://templatewebs.netlify.app/"],
+    origin: ["http://localhost:5173", "https://templatewebs.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
